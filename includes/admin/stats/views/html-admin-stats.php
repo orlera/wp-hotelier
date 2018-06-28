4bc4a5de4a34c17ea0c16a4386d7764b1f721e1e
Andrea Orler <andrea.orler@gmail.com>
2018-06-28 17:51:23 -0300

    -- fix date form

--- includes/admin/stats/views/html-admin-stats.php
+++ includes/admin/stats/views/html-admin-stats.php
@@ -18,20 +18,22 @@ $all_reservations = htl_get_all_reservations( $from->format('Y-m-d'), $to->forma
 <div class="wrap hotelier">
 
     <h1><?php esc_html_e( 'Stats', 'wp-hotelier' ); ?></h1>
-    <table class="stats-form-table">
-        <tr>
-            <th scope="row"><?php esc_html_e( 'Start date:', 'wp-hotelier' ); ?></th>
-            <td>
-                <input class="date-from" type="text" placeholder="YYYY-MM-DD" name="from" value="<?php echo esc_attr( $from->format('Y-m-d') ); ?>">
-            </td>
-            <th scope="row"><?php esc_html_e( 'End date:', 'wp-hotelier' ); ?></th>
-            <td>
-                <input class="date-to" type="text" placeholder="YYYY-MM-DD" name="to" value="<?php echo esc_attr( $to->format('Y-m-d') ); ?>">
-            </td>
-            <td>
-                <input class="submit" type="submit" name="submit" value="Show me the statzzz">
-            </td>
-        </tr>
-    </table>
-
+    <form action="<?php echo admin_url( 'admin.php' ); ?>" method="get" class="form form--bc">
+        <input type="hidden" name="page" value="hotelier-stats">
+        <table class="stats-form-table">
+            <tr>
+                <th scope="row"><?php esc_html_e( 'Start date:', 'wp-hotelier' ); ?></th>
+                <td>
+                    <input class="datepicker date-from" type="text" placeholder="YYYY-MM-DD" name="from" value="<?php echo esc_attr( $from->format('Y-m-d') ); ?>">
+                </td>
+                <th scope="row"><?php esc_html_e( 'End date:', 'wp-hotelier' ); ?></th>
+                <td>
+                    <input class="datepicker date-to" type="text" placeholder="YYYY-MM-DD" name="to" value="<?php echo esc_attr( $to->format('Y-m-d') ); ?>">
+                </td>
+                <td>
+                    <input class="button" type="submit" value="<?php esc_attr_e( 'Generate stats', 'wp-hotelier' ); ?>">
+                </td>
+            </tr>
+        </table>
+    </form>
 </div>
