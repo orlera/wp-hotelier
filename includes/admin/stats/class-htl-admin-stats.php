ab9e54ef37e6f69b1f44dc28bcc1ef9ee03013ed
Andrea Orler <andrea.orler@gmail.com>
2018-06-28 12:55:03 -0300

    -- clean&validate from/to params

--- includes/admin/stats/class-htl-admin-stats.php
+++ includes/admin/stats/class-htl-admin-stats.php
@@ -23,6 +23,27 @@ class HTL_Admin_Stats {
      * Show the view calendar page
      */
     public static function output() {
+    	// from date
+		$from_parameter = $_GET[ 'from' ];
+
+		$from = DateTime::createFromFormat('Y-m-d', $from_parameter);
+
+		// if from param is not valid - default to this month's first day
+		if ( $from === FALSE) {
+			$from = new DateTime('first day of this month');
+		}
+
+    	// to date
+		$to_parameter = $_GET[ 'to' ];
+
+		$to = DateTime::createFromFormat('Y-m-d', $to_parameter);
+
+		// if to param is not valid or gt from date - default to next month's first day
+		if ($to === FALSE || $to <= $from) {
+			$to = clone ($from);
+			$to->modify('+1 month')->modify('first day of this month');
+		}
+
         include_once( 'views/html-admin-stats.php' );
     }
 }
--- includes/admin/stats/views/html-admin-stats.php
+++ includes/admin/stats/views/html-admin-stats.php
@@ -7,6 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
+
+$all_reservations = htl_get_all_reservations( $from->format('Y-m-d'), $to->format('Y-m-d') );
+
+//var_dump($all_reservations);
+
+
 ?>
 
 <div class="wrap hotelier">
@@ -16,11 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) {
         <tr>
             <th scope="row"><?php esc_html_e( 'Start date:', 'wp-hotelier' ); ?></th>
             <td>
-                <input class="date-from" type="text" placeholder="YYYY-MM-DD" name="from" value="<?php echo esc_attr( $today ); ?>">
+                <input class="date-from" type="text" placeholder="YYYY-MM-DD" name="from" value="<?php echo esc_attr( $from->format('Y-m-d') ); ?>">
             </td>
             <th scope="row"><?php esc_html_e( 'End date:', 'wp-hotelier' ); ?></th>
             <td>
-                <input class="date-to" type="text" placeholder="YYYY-MM-DD" name="to" value="<?php echo esc_attr( $tomorrow ); ?>">
+                <input class="date-to" type="text" placeholder="YYYY-MM-DD" name="to" value="<?php echo esc_attr( $to->format('Y-m-d') ); ?>">
             </td>
             <td>
                 <input class="submit" type="submit" name="submit" value="Show me the statzzz">
